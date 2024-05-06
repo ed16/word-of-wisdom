fa6ae537472756b32b5710f4cbbf741d3de804c3
ed16 <e.zaynullin@gmail.com>
2024-05-06 22:05:50 +0200

    Make the challenge more comprehensive

--- pkg/pow/pow.go
+++ pkg/pow/pow.go
@@ -1,14 +1,40 @@
 package pow
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
+	"time"
 )
 
-// SolveChallenge solves the given Proof of Work challenge from the server.
+// IssueChallenge generates the Proof of Work challenge
+func IssueChallenge() string {
+	// Current time as a seed to ensure time-based uniqueness
+	timeSeed := []byte(time.Now().UTC().Format(time.RFC3339Nano))
+
+	// Generate a cryptographically secure random number
+	randomBytes := make([]byte, 16) // 128 bits
+	if _, err := rand.Read(randomBytes); err != nil {
+		log.Panic("Failed to generate random bytes: ", err)
+	}
+
+	// Combine time seed and random bytes
+	challengeBytes := append(timeSeed, randomBytes...)
+
+	// Hash the combined bytes to get a fixed size output
+	hash := sha256.Sum256(challengeBytes)
+
+	// Convert the hash to a hexadecimal string
+	challenge := hex.EncodeToString(hash[:])
+
+	return challenge
+}
+
+// SolveChallenge solves the given Proof of Work challenge from the server
 func SolveChallenge(challenge string, difficulty byte) string {
 	// Brute force search for the nonce
 	var nonce int
