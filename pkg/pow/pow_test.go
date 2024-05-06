37d5f41c8f0cab1a6e2c01bc751b4859e14cc151
ed16 <e.zaynullin@gmail.com>
2024-05-06 22:06:05 +0200

    Add test for the IssueChallenge

--- pkg/pow/pow_test.go
+++ pkg/pow/pow_test.go
@@ -7,6 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIssueChallenge(t *testing.T) {
+	// Create a set to hold challenges to test for uniqueness
+	challenges := make(map[string]bool)
+
+	for i := 0; i < 1000; i++ {
+		challenge := pow.IssueChallenge()
+		if len(challenge) != 64 { // SHA-256 hash is 64 hex characters
+			t.Errorf("Expected challenge length of 64, got %d", len(challenge))
+		}
+		if _, exists := challenges[challenge]; exists {
+			t.Errorf("Duplicate challenge found: %s", challenge)
+		}
+		challenges[challenge] = true
+	}
+}
+
 func TestValidateChallengePositive(t *testing.T) {
 	challenge := "testchallenge"
 	difficulty := byte(4)
