e302508912535f13dc4876400c03d50bdd6458dd
ed16 <e.zaynullin@gmail.com>
2024-05-06 22:32:53 +0200

    Increase coverage

--- internal/app/client/client_test.go
+++ internal/app/client/client_test.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/ed16/word-of-wisdom/config"
 	"github.com/ed16/word-of-wisdom/internal/app/client"
+	"github.com/ed16/word-of-wisdom/internal/app/server"
+	"github.com/ed16/word-of-wisdom/pkg/tcp"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -106,4 +109,24 @@ func TestConcurrentClientConnections(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
+}
+
+func TestServerClientInteraction(t *testing.T) {
+	serverConfig := &config.ServerConfig{
+		ListenAddr: "127.0.0.1:0",
+		Difficulty: 1,
+	}
+	srv := server.NewServer(serverConfig, &tcp.DefaultConnector{})
+	ctx, cancel := context.WithCancel(context.Background())
+	go srv.Start(ctx)
+	defer cancel()
+
+	clientConfig := &config.ClientConfig{
+		ServerAddr: "127.0.0.1:0",
+	}
+	clt := client.NewClient(clientConfig, &tcp.DefaultConnector{})
+
+	clt.Start(ctx)
+	assert.NotEmpty(t, srv)
+	assert.NotEmpty(t, clt)
 }
--- internal/app/server/handler_test.go
+++ internal/app/server/handler_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/ed16/word-of-wisdom/config"
+	"github.com/ed16/word-of-wisdom/internal/app/client"
 	"github.com/ed16/word-of-wisdom/internal/app/server"
 	"github.com/ed16/word-of-wisdom/pkg/tcp"
 	"github.com/stretchr/testify/assert"
@@ -80,4 +81,24 @@ func TestHandleConnection_InvalidPoW(t *testing.T) {
 	mockConnector.AssertExpectations(t)
 
 	assert.Contains(t, mockConn.buffer.String(), "Invalid PoW solution\n")
+}
+
+func TestServerClientInteraction(t *testing.T) {
+	serverConfig := &config.ServerConfig{
+		ListenAddr: "127.0.0.1:0",
+		Difficulty: 1,
+	}
+	srv := server.NewServer(serverConfig, &tcp.DefaultConnector{})
+	ctx, cancel := context.WithCancel(context.Background())
+	go srv.Start(ctx)
+	defer cancel()
+
+	clientConfig := &config.ClientConfig{
+		ServerAddr: "127.0.0.1:0",
+	}
+	clt := client.NewClient(clientConfig, &tcp.DefaultConnector{})
+
+	clt.Start(ctx)
+	assert.NotEmpty(t, srv)
+	assert.NotEmpty(t, clt)
 }
