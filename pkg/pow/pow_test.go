e30fa3f4113aa426fbbeffd73cf4d80256543a5e
ed16 <e.zaynullin@gmail.com>
2024-05-06 18:29:13 +0200

    Add negative test

--- pkg/pow/pow_test.go
+++ pkg/pow/pow_test.go
@@ -7,10 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateChallenge(t *testing.T) {
+func TestValidateChallengePositive(t *testing.T) {
 	challenge := "testchallenge"
 	difficulty := byte(4)
 	nonce := pow.SolveChallenge(challenge, difficulty)
 	valid := pow.ValidateChallenge(challenge, nonce, difficulty)
 	assert.True(t, valid)
+}
+
+func TestValidateChallengeFalse(t *testing.T) {
+	challenge := "testchallenge"
+	difficulty := byte(4)
+	nonce := "000001"
+	valid := pow.ValidateChallenge(challenge, nonce, difficulty)
+	assert.False(t, valid)
 }
